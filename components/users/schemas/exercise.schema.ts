f8767d2abd871b71397faa9d8123af68cb7a395e
Wilson Gouanet <wilstito@gmail.com>
2021-11-20 08:53:13 -0500

    set a getter for exercise date

--- components/users/schemas/exercise.schema.ts
+++ components/users/schemas/exercise.schema.ts
@@ -6,10 +6,22 @@ interface Exercise {
   date: string;
 }
 
-const ExerciseSchema = new Schema({
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  date: { type: Date, required: true, default: () => new Date() },
-});
+const ExerciseSchema = new Schema(
+  {
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: {
+      type: Date,
+      required: true,
+      default: () => new Date(),
+      get: (value: Date) => {
+        return value.toDateString();
+      },
+    },
+  },
+  {
+    toJSON: { getters: true },
+  },
+);
 
 export { ExerciseSchema, Exercise };
